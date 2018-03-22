79673967756a1a6583c6bf65170fd2fab35b9850
Nam Ly <nhl1285@gmail.com>
2018-03-21 23:58:41 -0700

    Solved reverse words in string

--- string/string_practice.c
+++ string/string_practice.c
@@ -2,37 +2,72 @@
 #include <stdlib.h>
 
 ///////////////////////////////////////////////////////////////////////////////
+// Reverse a single word
+//
+// @param str pointer to a string that contains word to reverse
+// @param start start index of the word to reverse
+// @end finish finish index of the word to reverse
+///////////////////////////////////////////////////////////////////////////////
+void reverseWord(char *str, int start, int finish)
+{
+    // Length of word to be reversed
+    unsigned int L = finish - start + 1;
+    for (unsigned int i = 0; i < L/2; i++)
+    {
+        char temp = str[start + i];
+        str[start + i] = str[finish - i];
+        str[finish - i] = temp;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // Reverse words in a string
 //
 // @param str string contains words to reverse
 ///////////////////////////////////////////////////////////////////////////////
 void reverseWords(char *str)
 {
-
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// Reverse a single word
-//
-// @param str pointer to a string that contains word to reverse
-// @param begin pointer to the begin of the word to reverse
-// @end end pointer to the end of the word to reverse
-///////////////////////////////////////////////////////////////////////////////
-void reverseWord(char *str, char *begin, char *end)
-{
-    // Length of word to be reversed
-    unsigned int L = end - begin;
-    for (unsigned int i = 0; i < L/2; i++)
+    unsigned int L = 0;
+    // Get size of input string
+    while(str[L++] != '\0')
     {
-        char temp = str[i];
-        str[i] = str[end - i - 1];
-        str[end - i - 1] = temp;
+    }
+    
+    // First reverse the string
+    reverseWord(str, 0, L - 2);
+
+    // Then reverse words in string
+    // Finish index of a word
+    unsigned int j = 0;
+    for (unsigned int i = 0; i < L - 1; )
+    {
+        // Skip whitespace
+        while(str[i] == ' ')
+        {
+            i++;
+        }
+        j = i;
+
+        // Skip non-whitespace
+        while(str[j] != ' ' && str[j] != '\0')
+        {
+            j++;
+        }
+        j -= 1;
+        // Reverse each word in string
+        reverseWord(str, i, j);
+        i = j + 1;
     }
 }
+
 
 int main()
 {
     printf("Play with string\n");
+    char str[] = "   Hello nam trang    ";
+    printf("Before: %s\n", str);
+    reverseWords(str);
+    printf("After: %s\n", str);
 
     return 0;
 }
